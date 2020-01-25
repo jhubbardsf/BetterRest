883bdcfb9472520714fdcbdd3deeb1e3ea8cacd9
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-24 21:01:46 -0500

    Add stepper and increment/decrement by .25 hours (15 minutes)

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
-        Text("Hello, World!")
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            // %.3g will show, at most 3 places and remove any access
+            Text("\(sleepAmount, specifier: "%.3g") hours")
+            
+        }
     }
 }
 
